0df57ddfe28e3fa2da2f627125142bde9c4b4863
mzayem <mzayemazam@gmail.com>
2025-03-14 16:31:44 +0500

    folder setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-pink-400 h-full">{children}</div>;
+};
+
+export default AuthLayout;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -5,8 +5,12 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-11">
-      <p className="text-center text-3xl text-indigo-500 font-bold">
+      <h1 className="text-center text-3xl text-indigo-500 font-bold">
         Hello Virtual Classroom
+      </h1>
+      <p>
+        this is a virtual classroom for learning purpose. you can join the class
+        and learn from the teacher.
       </p>
       <Button onClick={() => alert("hello class!")}>Join Class</Button>
     </div>
