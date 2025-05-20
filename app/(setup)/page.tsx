fcb28ce478c30b6d379fca64e96024884fe11899
mzayem <mzayemazam@gmail.com>
2025-05-20 19:40:49 +0500

    initial modal

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import InitialModal from "@/components/modal/initial-modal";
 import db from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -19,15 +20,5 @@ export default async function SetupPage() {
     return redirect(`/servers/${server.id}`);
   }
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen gap-11">
-      <h1 className="text-center text-3xl text-indigo-500 font-bold">
-        Create a Server
-      </h1>
-      <p>
-        this is a virtual classroom for learning purpose. you can join the class
-        and learn from the teacher.
-      </p>
-    </div>
-  );
+  return <InitialModal />;
 }
