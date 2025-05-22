5baf962efafe1a0cbd38d4eb1935c27e7271b5f0
mzayem <mzayemazam@gmail.com>
2025-05-22 17:46:32 +0500

    working on navigation

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,19 @@
+import NavigationSidebar from "@/components/navigation/navigation-sidebar";
+
+export default async function mainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full">
+      <div
+        className="hidden md:flex h-full w-[72px]
+            z-30 flex-col fixed inset-y-0"
+      >
+        <NavigationSidebar />
+      </div>
+      <main className="md:pl-20 h-full">{children}</main>
+    </div>
+  );
+}
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,32 @@
+import { currentProfile } from "@/lib/current-profile";
+import db from "@/lib/db";
+import { redirect } from "next/navigation";
+
+import NavigationAction from "./navigation-action";
+
+export default async function NavigationSidebar() {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  return (
+    <div
+      className="space-y-4 flex flex-col items-center
+        h-full text-primary w-full dark:bg-[#1E1F22]
+        py-3"
+    >
+      <NavigationAction />
+    </div>
+  );
+}
